f3922a6f24f792063baf6c696e7a8c51d13034d5
azxcv24 <azxcv24@users.noreply.github.com>
2022-05-30 23:01:10 +0900

    FIX autotoken 호출 URL 변경

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path, include
 from django.contrib import admin
-from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
 
     path('user/',include('Users.urls')),
-    path('api-token-auth/', obtain_auth_token),
+
 
     path('',include('UnityRankAPI.urls')),
 ]
