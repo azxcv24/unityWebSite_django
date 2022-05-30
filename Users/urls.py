704e6d987d3df533de20b60a7f9af1e9abe477e2
azxcv24 <azxcv24@users.noreply.github.com>
2022-05-30 23:24:05 +0900

    FIX autotoken 호출 URL 변경

--- Users/urls.py
+++ Users/urls.py
@@ -7,5 +7,5 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('signup/', views.UserCreate.as_view()),
     path('api-auth/', include('rest_framework.urls')),
-    path('api-token-auth/', obtain_auth_token),
+    path('api-auth-token/', obtain_auth_token),
 ]
